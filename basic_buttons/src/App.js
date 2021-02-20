8620493a95f529a9bbb94df4ccd9bf665de936f3
Harsh Deep <hrshdeep51@gmail.com>
2021-02-20 07:21:58 -0600

    add start webgazer button (not working)

--- basic_buttons/src/App.js
+++ basic_buttons/src/App.js
@@ -11,14 +11,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => webgazer.begin()}>
+          Turn on web gazer
+        </button>
       </header>
     </div>
   );
