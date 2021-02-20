684898160e37abbed729216319509452ba44cfc1
joannah2 <joannah2@illinois.edu>
2021-02-20 10:17:22 -0600

    Add from react tutorials through setState

--- basic_buttons/src/App.js
+++ basic_buttons/src/App.js
@@ -2,10 +2,23 @@ import webgazer from 'webgazer'
 
 import logo from './logo.svg';
 import './App.css';
+import { Greet } from './components/Greet';
+import Welcome from './components/Welcome';
+import Message from './components/Message';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
+      <Greet objName="Webgazer" targetName="ClassTranscribe"> 
+      <p>This is children props for functional componenet</p></Greet>
+
+      <Welcome objName="Webgazer" targetName="ClassTranscribe"> 
+      <p>This is children props for class component</p></Welcome>
+
+      <Message />
+      <Counter />
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,6 +32,9 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={() => webgazer.begin()}>
+          Turn on web gazer
+        </button>
       </header>
     </div>
   );
