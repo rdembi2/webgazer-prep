eaf2d62e3bf4f906f58de1f5c38be69e95db3957
Harsh Deep <hrshdeep51@gmail.com>
2021-04-17 09:26:08 -0500

    gaze cloud hacky page

---
+++ webgaze_pure_html/TODO.md
@@ -0,0 +1,27 @@
+# TODOs 
+
+* Get expand to work.
+
+* Have Next, Prev switch between apollo and sunset videos
+
+* Make this look a little nicer 
+
+* Pick a better video
+
+* Find prior lit
+
+* Set up presentations
+
+* Get one round of feedback from Angrave at least
+
+## Nice to have
+
+* See if we can make a `webgazer` hover thing on the other file so we can brag we have two things
+
+* React?? Too much effort proly 
+
+## Future Work
+
+* Actually integrate some real library like `webgazer`
+
+* https://github.com/szydej/GazeFlowAPI can give raw access to coordinates that we can work with
--- webgaze_pure_html/main.js
+++ webgaze_pure_html/main.js
@@ -1,39 +1,39 @@
-webgazer.begin()
+// webgazer.begin()
 
-let timeLookingAtScreen = 0;
-let prevStamp = 0;
+// let timeLookingAtScreen = 0;
+// let prevStamp = 0;
 
-const largeNumber = 525479; // can be whatever
-let minX = largeNumber;
-let minY = largeNumber; 
-let maxX = -largeNumber;
-let maxY = -largeNumber;
+// const largeNumber = 525479; // can be whatever
+// let minX = largeNumber;
+// let minY = largeNumber; 
+// let maxX = -largeNumber;
+// let maxY = -largeNumber;
 
-webgazer.setGazeListener(function(data, elapsedTime) {
-    if (data == null) {
-        return;
-    }
-    var xprediction = data.x; //these x coordinates are relative to the viewport
-    var yprediction = data.y; //these y coordinates are relative to the viewport
+// webgazer.setGazeListener(function(data, elapsedTime) {
+//     if (data == null) {
+//         return;
+//     }
+//     var xprediction = data.x; //these x coordinates are relative to the viewport
+//     var yprediction = data.y; //these y coordinates are relative to the viewport
 
-    timeLookingAtScreen += elapsedTime - prevStamp; // delta time
-    timeLookingAtScreen = elapsedTime;
+//     timeLookingAtScreen += elapsedTime - prevStamp; // delta time
+//     timeLookingAtScreen = elapsedTime;
 
-    if (xprediction < minX) {
-        console.log(`new min: ${xprediction} compared to ${minX}`);
-        minX = xprediction;
-    }
-    if (yprediction < minY) {
-        minY = yprediction;
-    }
-    if (xprediction > maxX) {
-        maxX = xprediction;
-    }
-    if (yprediction > maxY) {
-        maxY = yprediction;
-    }
-    // console.log(`${xprediction} ${yprediction} - ${elapsedTime}`); //elapsed time is based on time since begin was called
-}).begin();
+//     if (xprediction < minX) {
+//         console.log(`new min: ${xprediction} compared to ${minX}`);
+//         minX = xprediction;
+//     }
+//     if (yprediction < minY) {
+//         minY = yprediction;
+//     }
+//     if (xprediction > maxX) {
+//         maxX = xprediction;
+//     }
+//     if (yprediction > maxY) {
+//         maxY = yprediction;
+//     }
+//     // console.log(`${xprediction} ${yprediction} - ${elapsedTime}`); //elapsed time is based on time since begin was called
+// }).begin();
 
 
 function playpause(btn, vid) {
